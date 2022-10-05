10a6a23c5c739b43663ae8c2a1811c29bd8ab0ae
Allan Abner <allanabner_ti@hotmail.com>
2022-10-05 00:44:49 -0300

    Catalog API with MongoDB

--- src/Catalog/Catalog.API/Program.cs
+++ src/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,52 @@
+using Catalog.API.Data;
+using Catalog.API.Data.Interfaces;
+using Catalog.API.Repositories;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddScoped<ICatalogContext, CatalogContext>();
+services.AddScoped<IProductRepository, ProductRepository>();
+
+services.AddControllers();
+services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
+});
+
+services.AddHealthChecks()
+        .AddMongoDb(builder.Configuration["DatabaseSettings:ConnectionString"], "MongoDb Health", HealthStatus.Degraded);
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var env = app.Environment;
+
+if (env.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
+}
+
+app.UseRouting();
+app.UseAuthorization();
+
+
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+    {
+        Predicate = _ => true,
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+});
 
 app.Run();
